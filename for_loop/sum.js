c5760a0d9629e16a353b1f6a36c95b8c0520b676
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2021-07-26 12:13:07 +0600

    for loop-2

---
+++ for_loop/sum.js
@@ -0,0 +1,11 @@
+for(var x=1; x<=5; x++)
+{
+    var num1 = parseInt(prompt("Enter 1st number"));
+var num2 = parseInt(prompt("Enter 2nd number"));
+
+sum= num1+ num2
+console.log("Result = " + sum);
+    
+}
+
+
